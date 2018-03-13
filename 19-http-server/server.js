24b76ce4e2b5398c2032fdb031f3e2c55e6d7259
Melissa Bell <melissabell456@gmail.com>
2018-03-13 15:01:32 -0500

    completed exercise 19

---
+++ 19-http-server/server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const { readFile } = require('fs');
+const { createServer } = require('http');
+const server = createServer();
+server.on('request', (req, res) => {
+  readFile('./index.html', (err, file) => {
+    if (err) { res.statusCode = 404, res.end()}
+    res.end(file);
+  });
+})
+server.listen(8080)
